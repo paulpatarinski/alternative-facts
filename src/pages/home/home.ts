7fffc73ef30552fd6b93ca84ecbc01615f54db1a
Paul Patarinski <paulpatarinski@gmail.com>
2017-01-27 21:51:03 -0600

    Adding animations to the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,58 @@
-import { Component } from '@angular/core';
+import { Component , trigger, state, style, transition, animate} from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { GalleryPage } from '../gallery/gallery';
 import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+     trigger('flyInBottomSlow', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({transform: 'translate3d(0,2000px,0'}),
+        animate('2000ms ease-in-out')
+      ])
+    ]),
+      //For the background detail
+    trigger('flyInBottomMedium', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({transform: 'translate3d(0,2000px,0)'}),
+        animate('1500ms ease-in-out')
+      ])
+    ]),
+    //For the background detail
+    trigger('flyInBottomFast', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({transform: 'translate3d(0,2000px,0)'}),
+        animate('1000ms ease-in-out')
+      ])
+    ]),
+    trigger('flip', [
+      state('flipped', style({
+        transform: 'rotate(180deg)'
+      })),
+        state('notFlipped', style({
+        transform: 'rotate(-360deg)'
+      })),
+      transition('* => flipped', animate('400ms ease-in')),
+      transition('flipped => notFlipped', animate('400ms ease-out'))
+    ])
+  ]
 })
 
 export class HomePage {
   private _navCtrl : NavController;
- 
+   flipState: any;
+
   constructor(public navCtrl: NavController) {
     this._navCtrl = navCtrl;
   }
@@ -21,5 +63,9 @@ export class HomePage {
 
    navigateToAbout(){
    this.navCtrl.push(AboutPage);
+  }
+
+  toggleFlip() {
+    this.flipState = (this.flipState == 'notFlipped' || !this.flipState)  ? 'flipped' : 'notFlipped';
   }
 }
