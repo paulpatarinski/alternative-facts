fbf98ff2684fc1bdb8a8d056fbf81c9753d6f1d2
Paul Patarinski <paulpatarinski@gmail.com>
2017-01-28 11:21:37 -0600

    Tracking platform & version

--- src/providers/mixpanel-service.ts
+++ src/providers/mixpanel-service.ts
@@ -1,15 +1,23 @@
 import * as Mixpanel from "mixpanel";
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 @Injectable()
 export class MixpanelService {
     _mixPanel : any;
 
+    constructor(public plt: Platform) {
+        
+    }
+
     public init(token) {
-        this._mixPanel = Mixpanel.init(token, {});
+        this._mixPanel = Mixpanel.init(token, {protocol : 'https'});
     }
 
     track(event:string) {
-        this._mixPanel.track(event);
+        this._mixPanel.track(event, {
+            platform : this.plt.platforms(),
+            platformVersion : this.plt.versions()
+        });
     }
 }
