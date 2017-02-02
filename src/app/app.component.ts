1221ee19985e334a989d5ea3e1691b205d2d615f
Paul Patarinski <paulpatarinski@gmail.com>
2017-02-02 00:12:34 -0600

    Adding ids & starting calabash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,16 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-  constructor(platform: Platform, mixpanel : MixpanelService) {
+  constructor(platform: Platform, mixpanel: MixpanelService) {
     platform.ready().then(() => {
+      if ((<any>window).calabash) {
+        (<any>window).calabash.start(() => {
+          console.log('Calabash started')
+        }, (err) => {
+          console.log(err);
+        });
+      }
+
       mixpanel.init("8e61a5c5b1103da23c2088f2dc44a043");
 
       var statusBarOverlayWebView = platform.is('ios');
