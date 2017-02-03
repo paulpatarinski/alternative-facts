020c52c8eb67fc6aa8ca9e350407d99407f7f1f6
Paul Patarinski <paulpatarinski@gmail.com>
2017-02-02 23:14:51 -0600

    Pulling the settings out of the package json

--- ui_tests/AppInitializer.cs
+++ ui_tests/AppInitializer.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
+using Newtonsoft.Json;
+using System.Dynamic;
 
 namespace ui_tests
 {
@@ -10,40 +12,42 @@ namespace ui_tests
 	{
 		public static IApp StartApp(Platform platform)
 		{
-			// TODO: If the iOS or Android app being tested is included in the solution 
-			// then open the Unit Tests window, right click Test Apps, select Add App Project
-			// and select the app projects that should be tested.
-			//
-			// The iOS project should have the Xamarin.TestCloud.Agent NuGet package
-			// installed. To start the Test Cloud Agent the following code should be
-			// added to the FinishedLaunching method of the AppDelegate:
-			//
-			//    #if ENABLE_TEST_CLOUD
-			//    Xamarin.Calabash.Start();
-			//    #endif
+			var config = ParseConfig();
+
+
 			if (platform == Platform.Android)
 			{
 				return ConfigureApp
 					.Android
-					// TODO: Update this path to point to your Android app and uncomment the
-					// code if the app is not included in the solution.
-					//.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
 					.StartApp();
 			}
 
-			//iPhone 6 plus
-			// var deviceId = "00673C92-BEDA-445B-936E-5D811D6635EB";
-			//iPad Pro 12.9
-			var deviceId = "E958A769-F4AD-49F0-BA01-BED71D7536F6";
+			var deviceIdentifier = config.phone ? config.iphone_identifier : config.ipad_identifier;
 
 			return ConfigureApp
 				.iOS
-				// TODO: Update this path to point to your iOS app and uncomment the
-				// code if the app is not included in the solution.
-				.AppBundle("../../../platforms/ios/Build/emulator/Facts.app")
-				.DeviceIdentifier(deviceId)
+				.AppBundle(config.app_bundle_path)
+				.DeviceIdentifier(deviceIdentifier)
 				.EnableLocalScreenshots()
 				.StartApp();
+		}
+
+		private static Config ParseConfig()
+		{
+			return JsonConvert.DeserializeObject<PackageJson>(File.ReadAllText(@"../../package.json")).config;
+		}
+
+		public class PackageJson
+		{
+			public Config config { get; set; }
+		}
+
+		public class Config
+		{
+			public string app_bundle_path { get; set; }
+			public string iphone_identifier { get; set; }
+			public string ipad_identifier { get; set; }
+			public bool phone { get; set; }
 		}
 	}
 }
