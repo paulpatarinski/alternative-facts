c52119f73c11624a3bda280a4c0d9fb163477d6a
Paul Patarinski <paulpatarinski@gmail.com>
2017-02-02 23:51:06 -0600

    Removing unused namespaces

--- ui_tests/AppInitializer.cs
+++ ui_tests/AppInitializer.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Xamarin.UITest;
-using Xamarin.UITest.Queries;
 using Newtonsoft.Json;
-using System.Dynamic;
 
 namespace ui_tests
 {
